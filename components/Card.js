210aa54a0bdef117d0cfc65bf78520c5302a6d1a
i701 <idhaan701@outlook.com>
2021-10-11 00:44:00 +0500

    hoomans

--- components/Card.js
+++ components/Card.js
@@ -4,6 +4,26 @@ import tailwind from "tailwind-rn";
 // import NumericLabel from "react-native-numeric-label";
 
 const Card = (props) => {
+    var ranges = [
+        { divider: 1e18, suffix: "E" },
+        { divider: 1e15, suffix: "P" },
+        { divider: 1e12, suffix: "T" },
+        { divider: 1e9, suffix: "G" },
+        { divider: 1e6, suffix: "M" },
+        { divider: 1e3, suffix: "k" },
+    ];
+
+    function formatNumber(n) {
+        for (var i = 0; i < ranges.length; i++) {
+            if (n >= ranges[i].divider) {
+                return (
+                    (n / ranges[i].divider).toFixed(1).toString() +
+                    ranges[i].suffix
+                );
+            }
+        }
+        return n.toString();
+    }
     return (
         <View
             style={tailwind(
@@ -23,7 +43,8 @@ const Card = (props) => {
                 )}
             >
                 {/* <NumericLabel params={params}>`${props.data}`</NumericLabel> */}
-                {props.data}
+                {/* {props.data.toLocaleString("en")} */}
+                {formatNumber(props.data)}
             </Text>
         </View>
     );
